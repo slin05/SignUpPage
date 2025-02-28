786a64ad60337201806fc0d430f5b1b2a5928e93
slin05 <97693822+slin05@users.noreply.github.com>
2025-02-28 09:17:46 -0500

    Initial Files

---
+++ source/repos/SignUpPage/SignUpPage/Platforms/MacCatalyst/AppDelegate.cs
@@ -0,0 +1,9 @@
+﻿using Foundation;
+
+namespace SignUpPage;
+
+[Register("AppDelegate")]
+public class AppDelegate : MauiUIApplicationDelegate
+{
+	protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+}
