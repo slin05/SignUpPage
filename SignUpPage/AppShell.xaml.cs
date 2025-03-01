bcd52284850a048db2528c97e261572f3a34d2b5
slin05 <97693822+slin05@users.noreply.github.com>
2025-03-01 17:33:07 -0500

    Code Completed

--- SignUpPage/AppShell.xaml.cs
+++ SignUpPage/AppShell.xaml.cs
@@ -1,10 +1,11 @@
-﻿namespace SignUpPage
+﻿namespace SignUpPage;
+
+public partial class AppShell : Shell
 {
-    public partial class AppShell : Shell
+    public AppShell()
     {
-        public AppShell()
-        {
-            InitializeComponent();
-        }
+        InitializeComponent();
+
+        Routing.RegisterRoute("profile", typeof(Profile));
     }
 }
